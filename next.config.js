1658452d32c0d89dfa065855096232ee034dc594
Christopher Adekunle <72215147+Deoris06@users.noreply.github.com>
2023-01-28 01:22:34 +0100

    Added new URI

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
     reactStrictMode: false,
     publicRuntimeConfig: {
         APP_NAME: 'NODE-REACT-AWS',
-        API: 'http://localhost:8000/api',
+        API: 'https://linkify-api-xxpq.onrender.com/api',
         PRODUCTION: false,
-        DOMAIN: 'http://localhost:3000',
+        DOMAIN: 'https://linkify-ueri.onrender.com',
         FB_APP_ID: 'JJSLKADFLKSAHFDSLKL'
     }
    
